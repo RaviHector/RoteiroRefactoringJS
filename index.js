d4be1e5f4bb47ffaea32db589177ea1f6b991249
Rian-Rero <rian@dharma.com.br>
2025-11-08 13:07:35 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { readFileSync } = require("fs");
 
 function gerarFaturaStr(fatura, pecas) {
+  // função query
+  function getPeca(apresentacao) {
+    return pecas[apresentacao.id];
+  }
+
   // função extraída
-  function calcularTotalApresentacao(apre, peca) {
+  function calcularTotalApresentacao(apre) {
     let total = 0;
+    const peca = getPeca(apre);
     switch (peca.tipo) {
       case "tragedia":
         total = 40000;
@@ -33,15 +39,15 @@ function gerarFaturaStr(fatura, pecas) {
   }).format;
 
   for (let apre of fatura.apresentacoes) {
-    const peca = pecas[apre.id];
-    const total = calcularTotalApresentacao(apre, peca);
+    const total = calcularTotalApresentacao(apre);
 
     // créditos para próximas contratações
     creditos += Math.max(apre.audiencia - 30, 0);
-    if (peca.tipo === "comedia") creditos += Math.floor(apre.audiencia / 5);
+    if (getPeca(apre).tipo === "comedia")
+      creditos += Math.floor(apre.audiencia / 5);
 
     // mais uma linha da fatura
-    faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${
+    faturaStr += `  ${getPeca(apre).nome}: ${formato(total / 100)} (${
       apre.audiencia
     } assentos)\n`;
     totalFatura += total;
