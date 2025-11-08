cccf945872211871c56a57ad467e07cdc07dfae2
Rian-Rero <rian@dharma.com.br>
2025-11-08 13:21:16 -0300

    Commit 3 - Mais Extract Functions

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const { readFileSync } = require("fs");
 
 function gerarFaturaStr(fatura, pecas) {
-  // função query
+  // Função query
   function getPeca(apresentacao) {
     return pecas[apresentacao.id];
   }
 
-  // função extraída
+  // Função para formatar moeda
+  function formatarMoeda(valor) {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+      minimumFractionDigits: 2,
+    }).format(valor / 100);
+  }
+
+  // Função para calcular o total de cada apresentação
   function calcularTotalApresentacao(apre) {
     let total = 0;
     const peca = getPeca(apre);
@@ -29,6 +38,16 @@ function gerarFaturaStr(fatura, pecas) {
     }
     return total;
   }
+
+  // Função para calcular os Créditos
+  function calcularCredito(apre) {
+    let creditos = 0;
+    creditos += Math.max(apre.audiencia - 30, 0);
+    if (getPeca(apre).tipo === "comedia")
+      creditos += Math.floor(apre.audiencia / 5);
+    return creditos;
+  }
+
   let totalFatura = 0;
   let creditos = 0;
   let faturaStr = `Fatura ${fatura.cliente}\n`;
@@ -42,17 +61,15 @@ function gerarFaturaStr(fatura, pecas) {
     const total = calcularTotalApresentacao(apre);
 
     // créditos para próximas contratações
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (getPeca(apre).tipo === "comedia")
-      creditos += Math.floor(apre.audiencia / 5);
+    creditos += calcularCredito(apre);
 
     // mais uma linha da fatura
-    faturaStr += `  ${getPeca(apre).nome}: ${formato(total / 100)} (${
+    faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${
       apre.audiencia
     } assentos)\n`;
     totalFatura += total;
   }
-  faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`;
+  faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
   faturaStr += `Créditos acumulados: ${creditos} \n`;
   return faturaStr;
 }
